7083749b885bafaacf8f883f8e4d0c1659db2d99
Gene Blanchard <gblanchard4@gmail.com>
2016-03-14 21:59:43 -0500

    Better Functional Test!

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,37 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        # I notice the page title mentions todo lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # I am invited to enter a todo item
+
+        # I type "Buy peacock feathers"
+
+        # When I hit enter the page updates with "Buy peacock feathers" on a todo list
+
+        # There is another box inviting me to enter another item on a list
+        # I type "Tie a fly with peacock feathers"
+
+        # The page updates again, now shows both items
+
+        # I wonder if the site would remember my list. I see it has generated a unique
+        # URL for me with an explination
+
+        # I visit the URL and my list is still There
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
