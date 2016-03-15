cfb3fa0954deef6679d9edecc52f6dcb75886cfa
Gene Blanchard <gblanchard4@gmail.com>
2016-03-14 22:58:31 -0500

    Front page html now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         # I notice the page title mentions todo lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.asserIn('To-Do', header_text)
+        self.assertIn('To-Do', header_text)
 
         # I am invited to enter a todo item
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -35,7 +35,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peackock feathers' for row in rows)
+            any(row.text == '1: Buy peackock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is another box inviting me to enter another item on a list
