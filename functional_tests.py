1d10793a94bc4da77050976ff11d13727b62f1fa
Gene Blanchard <gblanchard4@gmail.com>
2016-03-14 22:44:11 -0500

    Functional test now checks on importing a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,16 +16,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # I notice the page title mentions todo lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.asserIn('To-Do', header_text)
 
         # I am invited to enter a todo item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # I type "Buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
 
         # When I hit enter the page updates with "Buy peacock feathers" on a todo list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peackock feathers' for row in rows)
+        )
 
         # There is another box inviting me to enter another item on a list
         # I type "Tie a fly with peacock feathers"
+        self.fail('Finish the test!')
 
         # The page updates again, now shows both items
 
