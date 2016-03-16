161820e0bdabab81dbcbba29a28ecf23d710dc08
Gene Blanchard <gblanchard4@gmail.com>
2016-03-15 21:51:47 -0500

    added new user and unique url checks

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -37,6 +37,8 @@ class NewVisitorTest(LiveServerTestCase):
         # When I hit enter the page updates with "Buy peacock feathers" on a todo list
         inputbox.send_keys(Keys.ENTER)
 
+        my_list_url = self.browser.current_url
+        self.assertRegex(my_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is another box inviting me to enter another item on a list
@@ -48,6 +50,33 @@ class NewVisitorTest(LiveServerTestCase):
         # The page updates again, now shows both items
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # A new user comes in, Francis
+
+        # With a new browser session to make sure no inforation is coming through
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page, there is no sign of my list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his own urlresolvers
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, my_list_url)
+
+        # No trace of my list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # I wonder if the site would remember my list. I see it has generated a unique
         # URL for me with an explination
